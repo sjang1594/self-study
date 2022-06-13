c618ffd0d23137bf979e8bb6d6087f0130be266f
seungho jang <sjang1594@gmail.com>
2022-06-14 00:35:43 +0900

    finish thread management

--- c++/concurrency/thread_management.cpp
+++ c++/concurrency/thread_management.cpp
@@ -14,11 +14,64 @@ class Fctor {
       }
 };
 
+// 2. What if I want to pass the parameter by value in functor
+class Fuctor2 {
+  public:
+      void operator()(string msg){
+        cout << "t1 says: "  << msg << endl;
+      }
+};
+
+// 3. What if I want to pass the parameter by reference in functor
+class Functor3{
+  public:
+      void operator()(string &msg){
+        cout << "t1 says: " << msg <<endl;
+      }
+};
+
 int main(){
   // Create Functor
   Fctor fct;
   std::thread t1(fct);
 
+  // 1. what if I just want to create t1 with arguments of Functor()
+  // std::thread t1(Fuctor()) --> This won't work (not compile)
+  // --> this is not creating a thread of t1 instead we are declaring a function
+  // --> with the function name t1. This function takes a single parameter
+  // --> which is a pointer to another function, and Fuctor takes no parameter
+  // --> and return a Functor, and a function t1 returns a thread
+  // SOLUTION: use another pair parenthesis
+  // std::thread t1((Fuctor()))
+  // explicitly that this is constructing a functor and pass it as parameter to the constructor t1
+
+  // 2. Passing the parameter through Functor (By value)
+  // string s = "Where there is no trust, there is no love";
+  // std::thread t1((Fctor2()), s)
+
+  // 3. Passing the parameter by reference
+  // if you pass the s to thread, it's always going to be 'pass-by-value'.
+  // if you really want to pass the string as reference, then use below
+  // std::thread t1((Fctor2()), std::ref(s))
+  // --> This will create a reference wrapper for s
+
+  // COMMENTS:
+  // The parent thread and child thread shared memeory which is string s
+  // Another way to achieve this, use the pointer
+  // Suppose, I do not want to share memory among threads --> data wast problem
+  // std::thread t1((Fctor()), std::move(s))
+  // --> this will move the s from the main thread to the child thread
+  // --> Efficient!!
+  // What if I want to do this
+  // Copy std::thread t2 = t1 --> Fail
+  // Then what can you do ?? (Transfer the owndership of a thread t1 to t2)
+  // std::thread t2 = std::move(t1);
+  // t2.join();
+  // --> Check thread
+  // cout << std::this_thread::get_id() << endl; --> This will print out the parent thread's ID
+  // cout <<t1.get_id() <<endl;
+
+  /* */
   // std::thread t1(function_1); //t1 starts running
   // Alternative Approcah - using RAII
   // Wrapper w(t1); --> when w goes out of scope, it will automatically join the thread
--- c++/concurrency/thread_management.md
+++ c++/concurrency/thread_management.md
@@ -5,5 +5,30 @@ thread_management.cpp
 
 1. Thread can be created using any callable object; Functor, function, and lambda function
 
+### Note
+```
+// The most vexing(annoying) syntax of c++
+  // what if I just want to create t1 with arguments of Functor()
+  std::thread t1(Fuctor()) //--> This won't work (not compile)
+  // --> this is not creating a thread of t1 instead we are declaring a function
+  // --> with the function name t1. This function takes a single parameter
+  // --> which is a pointer to another function, and Fuctor takes no parameter
+  // --> and return a Functor, and a function t1 returns a thread
+
+```c++
+
+* C++ standard says whenever a statement can be interpreted as a function decoration, it will be treated as function declaration
+
+### Question
+What if I have a very complicated problem to solve, then ideally I would have to create as many number of threads as the number of calls that I have, which also means that I want to create threads not more than hardware can support
+--> This is called **Oversubscription**
+
+Of course, Oversubscription is bad because there are more threads running than available CPU cores, it will creates a lot of context switching and too many of context switching will degrade the performance
+
+Solution to this,
+``std::thread::hardware_concurrency(); // Indication``
+This will guide for spliting tasks among the threads.
+
+
 ## Resource
 [Youtube](https://www.youtube.com/watch?v=f2nMqNj7vxE)
