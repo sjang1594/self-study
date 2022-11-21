c4e00d376248e36f7b85fd7e7437234168f3fd41
sjang1594 <sjang1594@gmail.com>
2022-11-21 23:10:50 +0900

    vector complete

--- GameDev/cpp/STL/vector_4.cpp
+++ GameDev/cpp/STL/vector_4.cpp
@@ -3,6 +3,75 @@
 using namespace std;
 
 template<typename T>
+class Iterator
+{
+public:
+    Iterator() : _ptr(nullptr)
+    {}
+
+    Iterator(T *ptr): _ptr(ptr)
+    {}
+
+    Iterator& operator++()
+    {
+        _ptr++;
+        return *this;
+    }
+
+    Iterator operator++(int)
+    {
+        Iterator temp = *this;
+        _ptr++;
+        return temp;
+    }
+
+    Iterator& operator--()
+    {
+        _ptr--;
+        return *this;
+    }
+
+    Iterator operator--(int)
+    {
+        Iterator temp = *this;
+        _ptr--;
+        return temp;
+    }
+
+    Iterator operator+(const int count)
+    {
+        Iterator temp = *this;
+        temp._ptr += count;
+        return temp;
+    }
+
+    Iterator operator-(const int count)
+    {
+        Iterator temp = *this;
+        temp._ptr -= count;
+        return temp
+    }
+
+    bool operator==(const Iterator& right)
+    {
+        return _ptr == right._ptr;
+    }
+
+    bool operator!=(const Iterator& right)
+    {
+        return !(*this == right);
+    }
+
+    T& operator*()
+    {
+        return *_ptr;
+    }
+
+public:
+    T *_ptr;
+};
+
+template<typename T>
 class Vector
 {
 public:
@@ -11,16 +80,57 @@ public:
 
     }
     ~Vector(){ if (_data) delete[] _data; }
-
-    int size(){return _size;}
-    int capacity(){return _capacity}
     
     void push_back(const T& val)
     {
         if (_size == _capacity)
         {
             int newCapacity = static_cast<int>(_capacity * 1.5);
+            if (newCapacity == _capacity)
+                newCapacity++;
+            
+            reserve(newCapacity);
         }
+
+        // 여유공간 - 데이터 저장
+        // [    ]
+        _data[_size] = val;
+        // 데이터 개수 증가
+        _size++;
+    }
+
+    void reserve(int capacity)
+    {
+        _capacity = capacity;
+
+        T* newData = new T[_capacity];
+        // 데이터 복사
+        for(int i=0; i < _size; i++)
+            newData[i] = _data[i];
+        
+        // 기존에 있던 데이터 날린다
+        if(_data)
+            delete[] _data;
+        
+        // 포인터 교체
+        _data = newData;
+    }
+
+    T& operator[](const int pos){return _data[pos];}
+    int size(){return _size;}
+    int capacity(){return _capacity}
+    void clear(){ _size=0; }
+
+public:
+    typedef Iterator<T> iterator;
+    
+    iterator begin()
+    {
+        return iterator(&_data[0]);
+    }
+    iterator end()
+    {
+        return begin() + _size;
     }
 
 private:
@@ -31,8 +141,7 @@ private:
 
 int main()
 {
-    vector<int> v;
-    
+    Vector<int> v;
     for (int i=0; i < 100; i++)
     {
         v.push_back(i);
@@ -46,9 +155,11 @@ int main()
 
     cout << "----" << endl;
 
-    for (vector<int>::iterator it=v.begin(); it!=v.end(); ++it)
+    for (Vector<int>::iterator it=v.begin(); it!=v.end(); ++it)
     {
         cout << (*it) << endl;
     }
+
+    v.clear();
     return 0;
 }
