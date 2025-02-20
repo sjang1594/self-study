0f7d9a21267bb30225c63505fbf76895acd91c6f
Seungho Jang <sjang1594@gmail.com>
2025-02-21 07:04:37 +0900

    update

---
+++ metal/Transformation/Transformation/TransformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformationApp.swift
+//  Transformation
+//
+//  Created by Seungho Jang on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TransformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
